4062d784bafdcd85d93eb18da00d30f32b82a048
Sallie McKee <salliemckee7@gmail.com>
2022-06-17 12:11:17 -0500

    initializing firebase backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello from index.js");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfif_oFBoC8AyYUbqfkwmt1gQKLufsr0k",
+  authDomain: "fir-9-dojo-3dffc.firebaseapp.com",
+  projectId: "fir-9-dojo-3dffc",
+  storageBucket: "fir-9-dojo-3dffc.appspot.com",
+  messagingSenderId: "803305953011",
+  appId: "1:803305953011:web:700aa2f0ab2a95451e4986",
+};
+
+initializeApp(firebaseConfig);
